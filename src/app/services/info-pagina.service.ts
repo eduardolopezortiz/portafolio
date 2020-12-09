940be82657ce68dce6b9c94d1bc74cfcf45f1efe
Brandon Lopez <eduardolopezortiz95@gmail.com>
2020-12-08 19:55:21 -0600

    carga de informacion desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
@@ -9,24 +9,40 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-  constructor(private http: HttpClient) {
 
+  equipo: any [] = [];
 
-    //console.log('servicio pagina listo');
+  constructor( private http: HttpClient ) {
 
-    //leer el archivo json
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe((resp: InfoPagina ) => {
-
-        this.cargada = true;
-        this.info = resp;
-
-        console.log(resp);
-
-
-      });
-
-
-
+    // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+   
   }
+
+  private cargarInfo(){
+     // Leer el archivo JSON
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe( (resp: InfoPagina) => {
+
+       this.cargada = true;
+       this.info = resp;
+      //  console.log(resp);
+     });
+  }
+//meotod para caragar la info de firebase a nuestra pagina
+  private cargarEquipo() {
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-7cf20-default-rtdb.firebaseio.com/.json')
+    .subscribe( (resp: any[]) => {
+
+     
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
+
+
+
