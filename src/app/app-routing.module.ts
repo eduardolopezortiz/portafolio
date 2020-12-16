d60fb32a63984f9388dc5f48b999fe0901549dac
Brandon Lopez <eduardolopezortiz95@gmail.com>
2020-12-16 03:50:30 -0600

    48 creando la pagina de busqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 
@@ -11,6 +12,7 @@ const app_routes: Routes =[
     {path: 'home', component: PortafolioComponent },
     {path: 'about', component: AboutComponent },
     {path: 'item/:id', component: ItemComponent },
+    {path: 'search/:termino', component: SearchComponent },
     {path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 
